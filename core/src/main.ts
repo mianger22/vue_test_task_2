c66d2f1487fe1d90978125578cc1dacf482c0794
mianger22 <test666777888999@yandex.ru>
2025-09-20 17:37:39 +0300

    Added pinia, composition api and uikit

--- core/src/main.ts
+++ core/src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
 
 createApp(App).mount('#app')
